556cdb39e69b5c0472118094f0d63a98a9695b1f
Sampson Cao <sampsontiet@gmail.com>
2025-12-28 18:08:11 -0500

    add BestTimeToBuyAndSellStock

--- src/BestTimeToBuyAndSellStock.java
+++ src/BestTimeToBuyAndSellStock.java
@@ -1,69 +1,23 @@
-import java.lang.reflect.Array;
-import java.util.Arrays;
-
 public class BestTimeToBuyAndSellStock {
 	public static int maxProfit(int[] prices) {
 
-		int[] localMaxes = new int[prices.length];
-		int[] localMins = new int[prices.length];
-
-		Arrays.fill(localMaxes, Integer.MIN_VALUE);
-		Arrays.fill(localMins, Integer.MAX_VALUE);
-
-		int tempMax = Integer.MIN_VALUE;
+		//int tempMax = Integer.MIN_VALUE;
 		int tempMin = Integer.MAX_VALUE;
-
-		for (int i = 0; i < prices.length; i++) {
-
-			if (prices[i] > tempMax) {
-				tempMax = prices[i];
-			}
-			else if (tempMax != Integer.MIN_VALUE){
-				localMaxes[i - 1] = tempMax;
-				tempMax = Integer.MIN_VALUE;
-			}
-
-			if (prices[i] < tempMin) {
-				tempMin = prices[i];
-			}
-			else if (tempMin != Integer.MAX_VALUE) {
-				localMins[i - 1] = tempMin;
-				tempMin = Integer.MAX_VALUE;
-			}
-		}
-		// edge case of the last price being a higher price/a maxima
-		localMaxes[prices.length - 1] = prices[prices.length - 1];
-
-		System.out.println("Local mins: " + Arrays.toString(localMins));
-		System.out.println("Local maxes: " + Arrays.toString(localMaxes));
-
 		int highestProfit = 0;
-		int buyPrice = 0;
-		boolean validTrade = false;
-		for (int i = 0; i < prices.length; i++) {
-			if (localMins[i] != Integer.MAX_VALUE) {
-				buyPrice = localMins[i];
-				validTrade = true;
-			}
-			if (validTrade) {
 
-				for (int j = i; j < prices.length; j++) {
+        for (int price : prices) {
 
-					if (localMaxes[j] == Integer.MIN_VALUE) {
-						continue;
-					}
+            int profit = price - tempMin;
+            System.out.println("Temp max: " + price + " Temp min: " + tempMin);
+            System.out.println("Profit: " + profit);
+            if (profit > highestProfit) {
+                highestProfit = profit;
+            }
 
-					// sell price - buy price
-					int profit = localMaxes[j] - buyPrice;
-
-					if (profit > highestProfit) {
-						highestProfit = profit;
-					}
-				}
-				validTrade = false;
-			}
-		}
-
+            if (price < tempMin) {
+                tempMin = price;
+            }
+        }
 		return highestProfit;
 	}
 
